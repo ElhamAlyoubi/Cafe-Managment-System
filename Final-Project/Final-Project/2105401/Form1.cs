62ace976057dfc607ef40f75a1b31dcaddc20b9c
ElhamAlyoubi <elhm1020@gmail.com>
2022-10-23 01:49:23 +0300

    ByElham

--- Final-Project/Final-Project/2105401/Form1.cs
+++ Final-Project/Final-Project/2105401/Form1.cs
@@ -19,7 +19,11 @@ namespace _2105401
 
         private void checkBox3_CheckedChanged(object sender, EventArgs e)
         {
-
+            if (checkBox3.Checked == true)
+            {
+                textBox3.Clear();
+                textBox3.Enabled = true;
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -41,28 +45,31 @@ namespace _2105401
         {
             txtCake1.Enabled = false;
             if (checkBox9.Checked == true)
+            {
+                txtCake1.Clear();
                 txtCake1.Enabled = true;
+            } 
         }
 
         private void checkBox10_CheckedChanged(object sender, EventArgs e)
         {
             txtCake2.Enabled = false;
             if (checkBox10.Checked == true)
-                txtCake2.Enabled = true;
+                txtCake2.Clear(); txtCake2.Enabled = true;
         }
 
         private void checkBox11_CheckedChanged(object sender, EventArgs e)
         {
             txtCake3.Enabled = false;
             if (checkBox11.Checked == true)
-                txtCake3.Enabled = true;
+                txtCake3.Clear(); txtCake3.Enabled = true;
         }
 
         private void checkBox12_CheckedChanged(object sender, EventArgs e)
         {
             txtCake4.Enabled = false;
             if (checkBox12.Checked == true)
-                txtCake4.Enabled = true;
+                txtCake4.Clear(); txtCake4.Enabled = true;
 
         }
 
@@ -70,28 +77,28 @@ namespace _2105401
         {
             txtCake5.Enabled = false;
             if (checkBox13.Checked == true)
-                txtCake5.Enabled = true;
+                txtCake5.Clear(); txtCake5.Enabled = true;
         }
 
         private void checkBox14_CheckedChanged(object sender, EventArgs e)
         {
             txtCake6.Enabled = false;
             if (checkBox14.Checked == true)
-                txtCake6.Enabled = true;
+                txtCake6.Clear(); txtCake6.Enabled = true;
         }
 
         private void checkBox15_CheckedChanged(object sender, EventArgs e)
         {
             txtCake7.Enabled = false;
             if (checkBox15.Checked == true)
-                txtCake7.Enabled = true;
+                txtCake7.Clear(); txtCake7.Enabled = true;
         }
 
         private void checkBox16_CheckedChanged(object sender, EventArgs e)
         {
             txtCake8.Enabled = false;
             if (checkBox16.Checked == true)
-                txtCake8.Enabled = true;
+                txtCake8.Clear(); txtCake8.Enabled = true;
         }
 
         private void txtCake1_TextChanged(object sender, EventArgs e)
@@ -136,6 +143,7 @@ namespace _2105401
 
         private void btnTotal_Click(object sender, EventArgs e)
         {
+           
             double Cake1 = Convert.ToDouble(txtCake1.Text);
             double Cake2 = Convert.ToDouble(txtCake2.Text);
             double Cake3 = Convert.ToDouble(txtCake3.Text);
@@ -152,11 +160,120 @@ namespace _2105401
             double Cake6_Cost = 19;
             double Cake7_Cost = 23;
             double Cake8_Cost = 23;
+            // TO Drinks
+            double deink1 = double.Parse(textBox1.Text);
+            double deink2 = double.Parse(textBox2.Text);
+            double deink3 = double.Parse(textBox3.Text);
+            double deink4 = double.Parse(textBox4.Text);
+            double deink5 = double.Parse(textBox5.Text);
+            double deink6 = double.Parse(textBox6.Text);
+            double deink7 = double.Parse(textBox7.Text);
+            double deink8 = double.Parse(textBox8.Text);
+
+            double drink1Cost = 10.1;
+            double drink2Cost = 10.00;
+            double drink3Cost = 10.65;
+            double drink4Cost = 10.2;
+            double drink5Cost = 10.1;
+            double drink6Cost = 10.90;
+            double drink7Cost = 10.8;
+            double drink8Cost = 10.5;
+
             double CostOfCakes = (Cake1_Cost * Cake1) +(Cake2_Cost * Cake2) + (Cake3_Cost * Cake3) +
                 (Cake4_Cost * Cake4) + (Cake5_Cost * Cake5) + (Cake6_Cost * Cake6) +
                 (Cake7_Cost * Cake7) + (Cake8_Cost * Cake8);
-            CostCakestextBox.Text = CostOfCakes.ToString();
+            CostCakestextBox.Text = "ر.س" + CostOfCakes.ToString();
+            double cost = ((deink1 * drink1Cost) +
+                           (deink2 * drink2Cost) +
+                           (deink3 * drink3Cost) +
+                           (deink4 * drink4Cost) +
+                           (deink5 * drink5Cost) +
+                           (deink6 * drink6Cost) +
+                           (deink7 * drink7Cost) +
+                           (deink8 * drink8Cost));
+                    CostDrinkstextBox.Text= "ر.س"+cost.ToString() ;
 
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+            if (checkBox1.Checked == true)
+            {
+                textBox1.Clear();
+                textBox1.Enabled = true;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+            if (checkBox2.Checked == true)
+            {
+                textBox2.Clear();
+                textBox2.Enabled = true;
+            }
+
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox4.Checked == true)
+            {
+                textBox4.Clear();
+                textBox4.Enabled = true;
+            }
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox5_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox5.Checked == true)
+            {
+                textBox5.Clear();
+                textBox5.Enabled = true;
+            }
+        }
+
+        private void checkBox6_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox6.Checked == true)
+            {
+                textBox6.Clear();
+                textBox6.Enabled = true;
+            }
+        }
+
+        private void checkBox7_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox7.Checked == true)
+            {
+                textBox7.Clear();
+                textBox7.Enabled = true;
+            }
+        }
+
+        private void checkBox8_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox8.Checked == true)
+            {
+                textBox8.Clear();
+                textBox8.Enabled = true;
+            }
         }
     }
 }
